4e5bcc7516d92d34c943ea5a42beec628e07063b
s1mcode <lifeisnotastruggle@gmail.com>
2024-04-17 16:55:27 +0800

    初始化项目

---
+++ readme.md
@@ -0,0 +1,13 @@
+## 配置
+
+```
+# 修改 my.cnf，设置 mysql 的用户名、密码、主机名
+vim my.cnf
+
+# 修改 run.sh，配置需要备份的数据库、备份文件存放目录
+vim run.sh
+
+# 将 my.cnf 文件的权限设置为只有所有者可以读写
+chmod 600 my.cnf
+```
+
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 要备份的数据库数组，用空格分隔多个数据库
+DATABASES=("database1" "database2")
+
+# 备份文件存放目录
+BACKUP_DIR="/path/to/your/backup/directory"
+
+# 日志文件路径
+LOG_FILE="${BACKUP_DIR}/backup_log_$(date +%Y%m%d).log"
+
+# 确保备份目录存在
+mkdir -p "${BACKUP_DIR}"
+
+# 循环备份每个数据库
+for DB in "${DATABASES[@]}"
+do
+    # 定义备份文件的完整路径，文件名以当前时间开头
+    BACKUP_FILE="${BACKUP_DIR}/$(date +%Y%m%d%H%M)_${DB}.sql"
+    # 获取当前时间
+    CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+    
+    # 执行备份命令并将输出结果重定向到日志文件
+    mysqldump --defaults-file=my.cnf "${DB}" > "${BACKUP_FILE}" 2>>"${LOG_FILE}"
+    
+    # 检查备份是否成功，并将结果与时间写入日志文件
+    if [ $? -eq 0 ]; then
+        echo "${CURRENT_TIME} - Database ${DB} backed up successfully." >> "${LOG_FILE}"
+    else
+        echo "${CURRENT_TIME} - Error in backing up database ${DB}" >> "${LOG_FILE}"
+    fi
+done
